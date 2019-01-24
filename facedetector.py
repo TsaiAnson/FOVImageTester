5856793dbb0c2fedc64a63f693dae1d7e0ae3ed9
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2019-01-24 14:41:06 -0800

    Migrated main method to function to allow inter-file executino

--- facedetector.py
+++ facedetector.py
@@ -15,22 +15,44 @@ def detect_face_cv_haarcascades (image_file):
 		faces = face_cascade.detectMultiScale(gray, 1.03, 5)
 
 		# For debugging
-		# for (x, y, w, h) in faces:
-		# 	cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+		for (x, y, w, h) in faces:
+			cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
 
-		# cv.imshow('img', img)
-		# cv.waitKey(0)
-		# cv.destroyAllWindows()
+		cv.imshow('img', img)
+		cv.waitKey(0)
+		cv.destroyAllWindows()
 
 		return faces
 
 	except:
 		return ''
 
+# Runs the face detection algorithms on single image (usually just for debugging)
+def run_face_detection_single (image_file):
+    # Creating Log File
+    log_file = open("face_detection_results.txt","w+")
+
+    log_file.write(str(detect_face_cv_haarcascades(image_file)) + '\n')
+
+    log_file.close()
+
+
+# Runs the face detection algorithms on a directory of images
+def run_face_detection_dir (image_dir):
+    # Creating Log File
+    log_file = open(join(image_dir, "face_detection_results.txt"),"w+")
+
+    files = [f for f in listdir(image_dir) if isfile(join(image_dir, f))]
+    for file in files:
+        file_dir = join(image_dir, file)
+        log_file.write(file_dir + ', ' + str(detect_face_cv_haarcascades(file_dir)) + '\n')
+
+    log_file.close()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_file", help="Path to Compressed Images.")
-    parser.add_argument("--image_dir", help="Path to Directory of Compressed Images")
+    parser.add_argument("--image_dir", default="results", help="Path to Directory of Compressed Images")
     args = parser.parse_args()
 
     if args.image_file is None and args.image_dir is None:
@@ -38,22 +60,9 @@ if __name__ == "__main__":
         exit()
 
     if args.image_file:
-    	# Creating Log File
-    	log_file = open("face_detection_results.txt","w+")
-
-    	log_file.write(str(detect_face_cv_haarcascades(args.image_file)) + '\n')
-
-    	log_file.close()
+    	run_face_detection_single(args.image_file)
 
     if args.image_dir:
-    	# Creating Log File
-    	log_file = open(join(args.image_dir, "face_detection_results.txt"),"w+")
-
-    	files = [f for f in listdir(args.image_dir) if isfile(join(args.image_dir, f))]
-    	for file in files:
-    		file_dir = join(args.image_dir, file)
-    		log_file.write(file_dir + ', ' + str(detect_face_cv_haarcascades(file_dir)) + '\n')
-
-    	log_file.close()
+    	run_face_detection_dir(args.image_dir)
 
     exit()
