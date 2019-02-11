18d552c858777d67f33d87c9f43c26d728d1f685
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2019-02-10 17:55:09 -0800

    Modified pipeline such that detected images are in different location than source

--- run_face_detection.py
+++ run_face_detection.py
@@ -12,19 +12,20 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     percentages = [float(value) for value in args.percentages.split(',')]
+    result_dir_temp = args.result_dir + '_temp'
 
     if args.image_file:
     	for perc in percentages:
-    		process_image(args.image_file, perc, args.result_dir)
+    		process_image(args.image_file, perc, result_dir_temp)
 
     if args.image_dir:
 
     	files = [join(args.image_dir, f) for f in listdir(args.image_dir) if isfile(join(args.image_dir, f))]
     	for file in files:
     		for perc in percentages:
-    			process_image(file, perc, args.result_dir)
+    			process_image(file, perc, result_dir_temp)
 
     # Face Detection
-    run_face_detection_dir(args.result_dir)
+    run_face_detection_dir(result_dir_temp, args.result_dir)
 
     exit()
