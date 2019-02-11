175dc247fd0a4dadae342a36c88a1eba53683f2d
Yuan-Cheng Tsai <ansonytsai@gmail.com>
2019-02-10 17:57:12 -0800

    Modified convert function for different methods of ratio fitting

--- imageconverter.py
+++ imageconverter.py
@@ -7,7 +7,22 @@ def convert_24_10 (pil_image):
     center_x = width // 2
     center_y = height // 2
 
-    # Using width as reference, will only cut height
+    # Mode 1: Keep Whole Face and add Black Bars
+    # # Uses the longer unit as reference (keeps original image and pads with zero)
+    # unit_width = width / 24.0
+    # unit_height = height / 10.0
+
+    # if unit_width > unit_height:
+    #     keep_height_half = int(unit_width * 5)
+        
+    #     cropped_image = pil_image.crop((0, center_y - keep_height_half, width, center_y + keep_height_half))
+    # else:
+    #     keep_width_half = int(unit_height * 12)
+
+    #     cropped_image = pil_image.crop((center_x - keep_width_half, 0, center_x + keep_width_half, height))
+
+    # Mode 2: Cut Face (More useful for eye detection)
+	# Using width as reference, will only cut height
     unit = width / 24.0
     keep_height_half = int(unit * 5)
 
@@ -37,8 +52,13 @@ def compress (pil_image):
     
     return resized_image
 
+def compress_m (pil_image, w, h):
+    resized_image = pil_image.resize((w, h))
+    
+    return resized_image
 
-# Driver Method
+
+# Driver Method (returns temporary directory of images)
 def process_image (image_file, percentage, result_path):
     # Creates results directory if it doesn't exist
     file_path = "./%s/" % result_path
@@ -48,7 +68,7 @@ def process_image (image_file, percentage, result_path):
             os.makedirs(directory)
     except Exception as e:
         print("Unable to create results directory. %s" % str(e))
-        exit()
+        exit ()
 
     # Opening Image
     try:
@@ -66,8 +86,36 @@ def process_image (image_file, percentage, result_path):
     pil_image = reduce_p(pil_image, percentage)
     # pil_image.save(results_image_name + '_reduce_%f.png' % percentage, 'PNG')
 
-    pil_image = compress(pil_image)
-    pil_image.save(results_image_name + '_compress_%f.png' % percentage, 'PNG')
+    compressed_image = compress(pil_image)
+    compressed_image.save(results_image_name + '_compress_%f.png' % percentage, 'PNG')
+
+    # # For Testing purposes (saving images at different resolutions)
+    # compressed_image = compress_m(pil_image, 48, 20)
+    # compressed_image.save(results_image_name + '_compress48x20_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 72, 30)
+    # compressed_image.save(results_image_name + '_compress72x30_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 96, 40)
+    # compressed_image.save(results_image_name + '_compress96x40_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 120, 50)
+    # compressed_image.save(results_image_name + '_compress120x50_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 144, 60)
+    # compressed_image.save(results_image_name + '_compress144x60_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 168, 70)
+    # compressed_image.save(results_image_name + '_compress168x70_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 192, 80)
+    # compressed_image.save(results_image_name + '_compress192x80_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 216, 90)
+    # compressed_image.save(results_image_name + '_compress216x90_%f.png' % percentage, 'PNG')
+
+    # compressed_image = compress_m(pil_image, 240, 100)
+    # compressed_image.save(results_image_name + '_compress240x100_%f.png' % percentage, 'PNG')
 
     return
 
